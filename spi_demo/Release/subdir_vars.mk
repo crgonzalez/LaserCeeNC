b8f67f7b31f350a49cd7a59df611ff8ea567e3bf
Isaac Lindgren <49lindgren@gmail.com>
2016-03-12 17:25:32 -0600

    Basic SPI Setup
    This is the first working spi setup. You can basically hardcode bytes to
    send over spi. More to come soon

---
+++ spi_demo/Release/subdir_vars.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../cc3200v1p32.cmd 
+
+C_SRCS += \
+../main.c \
+../pinmux.c \
+C:/TI/CC3200SDK_1.1.0/cc3200-sdk/example/common/startup_ccs.c \
+C:/TI/CC3200SDK_1.1.0/cc3200-sdk/example/common/uart_if.c 
+
+OBJS += \
+./main.obj \
+./pinmux.obj \
+./startup_ccs.obj \
+./uart_if.obj 
+
+C_DEPS += \
+./main.pp \
+./pinmux.pp \
+./startup_ccs.pp \
+./uart_if.pp 
+
+C_DEPS__QUOTED += \
+"main.pp" \
+"pinmux.pp" \
+"startup_ccs.pp" \
+"uart_if.pp" 
+
+OBJS__QUOTED += \
+"main.obj" \
+"pinmux.obj" \
+"startup_ccs.obj" \
+"uart_if.obj" 
+
+C_SRCS__QUOTED += \
+"../main.c" \
+"../pinmux.c" \
+"C:/TI/CC3200SDK_1.1.0/cc3200-sdk/example/common/startup_ccs.c" \
+"C:/TI/CC3200SDK_1.1.0/cc3200-sdk/example/common/uart_if.c" 
+
+
