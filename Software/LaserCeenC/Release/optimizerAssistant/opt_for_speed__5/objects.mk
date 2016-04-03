87dd47513ed168c0a7aa88872837305e2418b32c
ZLaronM <zacharius.mitchell@gmail.com>
2016-04-03 16:42:34 -0500

    Updated webpage to processing code

---
+++ Software/LaserCeenC/Release/optimizerAssistant/opt_for_speed__5/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lL6472.obj -lLaserBoard.obj -l"libc.a" -lsimplelink.a -ldriverlib.a -lfree_rtos.a -lgcode.obj
+
---
+++ Software/LaserCeenC/Release/optimizerAssistant/opt_for_speed__5/subdir_vars.mk
@@ -0,0 +1,63 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../cc3200v1p32.cmd 
+
+C_SRCS += \
+../fileparse.c \
+C:/ti/CC3200SDK_1.1.0/cc3200-sdk/example/common/gpio_if.c \
+../main.c \
+../pinmux.c \
+../smartconfig.c \
+C:/ti/CC3200SDK_1.1.0/cc3200-sdk/example/common/startup_ccs.c \
+C:/ti/CC3200SDK_1.1.0/cc3200-sdk/example/common/uart_if.c 
+
+OBJS += \
+./fileparse.obj \
+./gpio_if.obj \
+./main.obj \
+./pinmux.obj \
+./smartconfig.obj \
+./startup_ccs.obj \
+./uart_if.obj 
+
+C_DEPS += \
+./fileparse.pp \
+./gpio_if.pp \
+./main.pp \
+./pinmux.pp \
+./smartconfig.pp \
+./startup_ccs.pp \
+./uart_if.pp 
+
+C_DEPS__QUOTED += \
+"fileparse.pp" \
+"gpio_if.pp" \
+"main.pp" \
+"pinmux.pp" \
+"smartconfig.pp" \
+"startup_ccs.pp" \
+"uart_if.pp" 
+
+OBJS__QUOTED += \
+"fileparse.obj" \
+"gpio_if.obj" \
+"main.obj" \
+"pinmux.obj" \
+"smartconfig.obj" \
+"startup_ccs.obj" \
+"uart_if.obj" 
+
+C_SRCS__QUOTED += \
+"../fileparse.c" \
+"C:/ti/CC3200SDK_1.1.0/cc3200-sdk/example/common/gpio_if.c" \
+"../main.c" \
+"../pinmux.c" \
+"../smartconfig.c" \
+"C:/ti/CC3200SDK_1.1.0/cc3200-sdk/example/common/startup_ccs.c" \
+"C:/ti/CC3200SDK_1.1.0/cc3200-sdk/example/common/uart_if.c" 
+
+
